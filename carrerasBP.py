c4764c7f1535c75f25a65dcad963ea8da3303106
Emmanuel <ehurtado369ccitesz.com>
2023-05-18 17:38:47 -0600

    version 3

--- carrerasBP.py
+++ carrerasBP.py
@@ -1,26 +1,49 @@
 from flask import Blueprint, request
 from model import Conexion
+from flask_httpauth import HTTPBasicAuth
 
 carreraBP=Blueprint("carreraBP", __name__)
+auth=HTTPBasicAuth()
+
+@auth.verify_password
+def login(username, password):
+    cn=Conexion()
+    user=cn.validarCredenciales(username, password)
+    if user!=None:
+        return user
+    else:
+        return False
+
+@auth.get_user_roles
+def get_user_roles(user):
+    return user["tipo"]
+
+@auth.error_handler
+def error_handler():
+    return {"estatus":"Error", "mensaje":"Autorizacion denegana, usted no cuenta con los permisos necesarios para realizar esta accion"}
+
+@carreraBP.route('/Carreras',methods=['GET'])
+@auth.login_required(role=['A', 'E', 'D'])
+def consultaCarreras():
+    cn=Conexion()
+    return cn.consultarCarreras()
 
 @carreraBP.route('/Carreras',methods=['POST'])
+@auth.login_required(role='A')
 def agregarCarrera():
     cn=Conexion()
     datos=request.get_json()
     return cn.insertar_carrera(datos)
 
-@carreraBP.route('/Carreras',methods=['GET'])
-def ConsultaCarreras():
-    cn=Conexion()
-    return cn.consultarCarreras()
-
 @carreraBP.route("/Carreras", methods=['PUT'])
+@auth.login_required(role='A')
 def modificarCarrera():
     cn = Conexion()
     datos = request.get_json()
     return cn.modificarCarrera(datos)
 
-@carreraBP.route('/Carreras/<string:id>', methods=['DELETE'])
+@carreraBP.route('/Carreras/<int:id>', methods=['DELETE'])
+@auth.login_required(role='A')
 def eliminarCarrera(id):
     cn=Conexion()
     return cn.eliminarCarrera(id)
--- model.py
+++ model.py
@@ -8,13 +8,7 @@ class Conexion():
         self.col=self.bd.carreras
         self.col2=self.bd.planesEstudio
 
-    def insertar_carrera(self, carrera):
-        respuesta = {"Estatus": "", "Mensaje": ""}
-        carrera["estatus"] = "A"
-        self.col.insert_one(carrera)
-        respuesta["Estatus"] = "OK"
-        respuesta["Mensaje"] = "Carrera agregada correctamente"
-        return respuesta
+    #FUNCIONES DEL MODULO CARRERAS
 
     def consultarCarreras(self):
         resp = {"estatus": "", "mensaje": ""}
@@ -31,11 +25,20 @@ class Conexion():
             resp["mensaje"] = "No hay carreras registradas"
         return resp
 
+    def insertar_carrera(self, carrera):
+        respuesta = {"Estatus": "", "Mensaje": ""}
+        carrera["estatus"] = "A"
+        self.col.insert_one(carrera)
+        respuesta["Estatus"] = "OK"
+        respuesta["Mensaje"] = "Carrera agregada correctamente"
+        return respuesta
+
     def modificarCarrera(self, data):
         resp = {"estatus:": "", "mensaje:": ""}
         res = self.bd.carreras.find_one({"_id": data["_id"]})
+        print(res)
         if res:
-            self.bd.carreras.update_one({"_id": data["_id"]}, {"$set": res})
+            self.bd.carreras.update_one({"_id": data["_id"]}, {"$set": data})
             resp["estatus:"] = "OK"
             resp["mensaje:"] = "Se actualizo la carrera correctamente"
         else:
@@ -45,8 +48,9 @@ class Conexion():
 
     def eliminarCarrera(self, id):
         resp = {"estatus": "", "mensaje": ""}
-        res = self.bd.carreras.delete_one({"_id": ObjectId(id), "estatus": "I"})
-        if res.delete_count > 0:
+        res = self.bd.carreras.delete_one({"_id": id})
+        print(res)
+        if res.deleted_count > 0:
             resp["estatus"] = "OK"
             resp["mensaje"] = "La carrera se elimino con exito"
         else:
@@ -54,7 +58,9 @@ class Conexion():
             resp["mensaje"] = "La carrera no existe o no se encuentra en estatus de Inactiva"
         return resp
 
-#Planes de estudio
+
+
+    # FUNCIONES DEL MODULO PLANES DE ESTUDIO
     def insertar_planEstudio(self, planesEstudio):
         respuesta = {"Estatus": "", "Mensaje": ""}
         planesEstudio["estatus"] = "A"
@@ -77,3 +83,20 @@ class Conexion():
             resp["estatus"] = "OK"
             resp["mensaje"] = "No hay Planes De Estudio Registrados"
         return resp
+
+
+
+    # FUNCIONES DEL MODULO ESPECIALIDADES
+
+
+
+    # FUNCIONES DEL MODULO ASIGNATURAS
+
+
+    #FUNCIONES APARTE
+    def validarCredenciales(self, usuario, password):
+        user = self.bd.usuarios.find_one({"email": usuario, "password": password, "estatus": "A"})
+        if user:
+            return user
+        else:
+            return None
